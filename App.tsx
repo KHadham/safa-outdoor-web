a480fc29f1f4dc9bc009ae4146a28cddbd52bcc6
Khadam ikhwanus shofa <kampretosjr@gmail.com>
2024-03-02 18:44:04 +0700

    tes

--- App.tsx
+++ App.tsx
@@ -1,25 +1,6 @@
 import App from "./app/app"
 import React from "react"
 import * as SplashScreen from "expo-splash-screen"
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA7-Boev9OcD-9X8ptaf_gL5zog1Set3AI",
-  authDomain: "safa-outdoors.firebaseapp.com",
-  projectId: "safa-outdoors",
-  storageBucket: "safa-outdoors.appspot.com",
-  messagingSenderId: "665786080620",
-  appId: "1:665786080620:web:9ba2a697e04d4a1259a749",
-  measurementId: "G-0E8LVH3WNR"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 SplashScreen.preventAutoHideAsync()
 
